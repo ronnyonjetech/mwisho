9b519da8dc8cfa7fead21afcf8f9666124c10286
ronnyonjetech <63746922+ronnyonjetech@users.noreply.github.com>
2022-01-25 18:21:52 +0300

    second commit

--- MwishoWangu/settings.py
+++ MwishoWangu/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hq9xf=bv80iji3*o9r)u0q!4@vvfr_hrsvk2l9m_pw@n!8px*-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mwishowangu.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
